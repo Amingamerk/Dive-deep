c3838335b5251a6fe6fe4e71103c4174d5d2f85a
Joachim Thomsen <Joachimthomsen@outlook.dk>
2026-09-01 11:44:39 +0200

    Added logic for Product class and a Enum without Data

--- Dive-deep/Models/Product.cs
+++ Dive-deep/Models/Product.cs
@@ -1,6 +1,20 @@
-﻿namespace Dive_deep.Models
+﻿using static Dive_deep.Models.Enums;
+
+namespace Dive_deep.Models
+  
+    
 {
-    public class Product
+    public abstract class Product
     {
+        public int Id { get; set; }
+        public string Brand { get; set; } = "";
+        public string? Model { get; set; } = "";
+        public float PricePerDay { get; set; }
+        public ProductCategory Category { get; set; }
+       
+
+        public virtual IEnumerable<string> SizeOptions => Enumerable.Empty<string>();
+        public virtual string SizeLabel => "Størrelse"; 
+
     }
 }
