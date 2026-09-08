01637a8913c295d598cdab2f2f3fa7c03b06e373
Amin Christiansen <Amin081204@gmail.com>
2026-09-08 13:53:12 +0200

    Implement ProductRepository and update Details view
    Added `GetAll` and `GetById` methods to `IProductRepository` and implemented them in `ProductRepository` using `DiveDeepContext`. Updated `Details.cshtml` to include a dynamic form for product details and booking functionality.

--- DiveDeep/Persistence/IProductRepository.cs
+++ DiveDeep/Persistence/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DiveDeep.Persistence
+﻿using DiveDeep.Models;
+
+namespace DiveDeep.Persistence
 {
     public interface IProductRepository
     {
+        //void Add
+        //void Delete(int id);
+        List<Product> GetAll();
+        Product? GetById(int id);
+        //void Update
     }
 }
--- DiveDeep/Persistence/ProductRepository.cs
+++ DiveDeep/Persistence/ProductRepository.cs
@@ -1,7 +1,35 @@
-﻿namespace DiveDeep.Persistence
+﻿using DiveDeep.Models;
+using DiveDeep.Data;
+
+namespace DiveDeep.Persistence
 {
     public class ProductRepository : IProductRepository
     {
+        private DiveDeepContext _diveDeepContext;
 
+        public ProductRepository(DiveDeepContext diveDeepContext)
+        {
+            _diveDeepContext = diveDeepContext;
+        }
+
+
+        public List<Product> GetAll()
+        {
+            _diveDeepContext.Database.EnsureCreated();
+            return _diveDeepContext.Products.ToList();
+        }
+
+        public Product? GetById(int id)
+        {
+            _diveDeepContext.Database.EnsureCreated();
+
+            var product = _diveDeepContext.Products.FirstOrDefault(p => p.ProductId == id);
+            if (product == null)
+            {
+                //TODO
+            }
+            return product;
+
+        }
     }
 }
