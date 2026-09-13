10fa206ef1d3f36b63a03caa6bee5bcad0a3e544
Lauge <lauge2000@gmail.com>
2026-09-13 15:04:46 +0200

    - First steps of adding an admin panel - Added shared layouts for adminpanels, with a sidebar

---
+++ DiveDeep/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using DiveDeep.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiveDeep.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
